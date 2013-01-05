104a297f28cc09416513ecdde93e8496cc3fc1c8
JXPheonix <jxpheonix@gmail.com>
2013-01-05 18:47:39 -0500

    gui method (?)

---
+++ src/xphnx/ircat/BotHandler.java
@@ -0,0 +1,15 @@
+package xphnx.ircat;
+
+public class BotHandler {
+
+	public void inquire() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void gui() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/xphnx/ircat/Main.java
+++ src/xphnx/ircat/Main.java
@@ -1,5 +1,15 @@
 package xphnx.ircat;
 
 public class Main {
+	
+	public static void main(String args[]){
+		//PIRCbot! It's useful.
+		
+		//Start a new bot handler
+		BotHandler Hanley = new BotHandler();
+		
+		//Hanley, start your GUI
+		Hanley.gui();
+	}
 
 }
